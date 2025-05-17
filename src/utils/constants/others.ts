f70ca6e9dfab5058886a85d0479e15f12e81c1ce
Rosgen Hizer <rokhai.dev@gmail.com>
2025-05-17 15:26:13 +0800

    chore: add constants list for socials, techonologies

---
+++ src/utils/constants/others.ts
@@ -0,0 +1,112 @@
+import {
+    AiOutlineMail,
+    AiFillGithub,
+    AiFillLinkedin
+} from "react-icons/ai";
+
+import { FaHtml5, 
+    FaCss3Alt, 
+    FaJsSquare, 
+    FaReact, 
+    FaPhp, 
+    FaPython, 
+    FaNodeJs, 
+    FaLaravel, 
+    FaBootstrap, 
+    FaGitAlt, 
+    FaDocker } from "react-icons/fa";
+
+import { 
+    SiTypescript, 
+    SiJquery, 
+    SiExpress, 
+    SiMysql, 
+    SiMongodb, 
+    SiTailwindcss, } from "react-icons/si";
+
+
+export const socials = [
+    {
+        url: 'mailto:hizerrosgen@gmail.com',
+        Icon: AiOutlineMail
+    },
+    {
+        url: 'https://github.com/Rokhai',
+        Icon: AiFillGithub
+    },
+    {
+        url: 'https://linkedin.com/in/rosgenhizer',
+        Icon: AiFillLinkedin
+    }
+]
+
+export const technologies = [
+    {
+        name: 'HTML',
+        Icon: FaHtml5
+    },
+    {
+        name: 'CSS',
+        Icon: FaCss3Alt
+    },
+    {
+        name: 'JavaScript',
+        Icon: FaJsSquare
+    },
+    {
+        name: 'jQuery',
+        Icon: SiJquery
+    },
+    {
+        name: 'TypeScript',
+        Icon: SiTypescript
+    },
+    {
+        name: 'React',
+        Icon: FaReact
+    },
+    {
+        name: 'Node.js',
+        Icon: FaNodeJs
+    },
+    {
+        name: 'Express.js',
+        Icon: SiExpress
+    },
+    {
+        name: 'PHP',
+        Icon: FaPhp
+    },
+    {
+        name: 'Python',
+        Icon: FaPython
+    },
+    {
+        name: 'MySQL',
+        Icon: SiMysql
+    },
+    {
+        name: 'MongoDB',
+        Icon: SiMongodb
+    },
+    {
+        name: 'Laravel',
+        Icon: FaLaravel
+    },
+    {
+        name: 'Bootstrap',
+        Icon: FaBootstrap
+    },
+    {
+        name: 'Tailwind CSS',
+        Icon: SiTailwindcss
+    },
+    {
+        name: 'Git',
+        Icon: FaGitAlt
+    },
+    {
+        name: 'Docker',
+        Icon: FaDocker
+    }
+]
