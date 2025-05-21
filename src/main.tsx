df8e2e3b4052cf12eab4d536eaeb3c7e54cfb679
Rosgen Hizer <rokhai.dev@gmail.com>
2025-05-21 11:22:31 +0800

    feat: apply the MouseRadialTorch

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import {MouseRadialTorch} from './components/'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <MouseRadialTorch />
     <App />
   </StrictMode>,
 )
