05f355ea12645f9892d35e346b3538f57aa9bb5b
Rosgen Hizer <rokhai.dev@gmail.com>
2025-05-21 11:14:21 +0800

    feat: add Experience section component

--- src/components/Sections/Technologies.tsx
+++ src/components/Sections/Technologies.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import { technologies } from '../../utils/constants';
 
-const Technologies: React.FC = () => {
+export const Technologies: React.FC = () => {
 
     return (
-        <section id='technologies' className=''>
-            <h2 className='text-accent font-medium text-xl mb-3'>Technologies</h2>
+        <section id='technologies' className='my-24 sm:my-32'>
+            <h2 className='section-title'>Technologies</h2>
             <ul className='inline-flex flex-wrap gap-2'>
                 {technologies.map(({name, Icon}) => (
                     <li key={name}
-                        className='flex items-center space-x-2 px-3 rounded-full py-2 bg-accent/10'>
+                        className='flex items-center space-x-2 px-3 rounded-full py-2 bg-accent-muted'>
                         <Icon className='text-secondary' />
                         <span className='text-secondary text-xs'>{name}</span>
                     </li>
@@ -19,5 +19,3 @@ const Technologies: React.FC = () => {
         </section>
     );
 };
-
-export default Technologies;
