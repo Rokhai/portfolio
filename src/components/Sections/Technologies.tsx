cb7c6b5ddc1518e5a4049022551c651e6d6159a9
Rosgen Hizer <rokhai.dev@gmail.com>
2025-05-21 11:16:16 +0800

    Revert "feat: add Experience section component" revert: change the commit message This reverts commit 05f355ea12645f9892d35e346b3538f57aa9bb5b.

--- src/components/Sections/Technologies.tsx
+++ src/components/Sections/Technologies.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import { technologies } from '../../utils/constants';
 
-export const Technologies: React.FC = () => {
+const Technologies: React.FC = () => {
 
     return (
-        <section id='technologies' className='my-24 sm:my-32'>
-            <h2 className='section-title'>Technologies</h2>
+        <section id='technologies' className=''>
+            <h2 className='text-accent font-medium text-xl mb-3'>Technologies</h2>
             <ul className='inline-flex flex-wrap gap-2'>
                 {technologies.map(({name, Icon}) => (
                     <li key={name}
-                        className='flex items-center space-x-2 px-3 rounded-full py-2 bg-accent-muted'>
+                        className='flex items-center space-x-2 px-3 rounded-full py-2 bg-accent/10'>
                         <Icon className='text-secondary' />
                         <span className='text-secondary text-xs'>{name}</span>
                     </li>
@@ -19,3 +19,5 @@ export const Technologies: React.FC = () => {
         </section>
     );
 };
+
+export default Technologies;
